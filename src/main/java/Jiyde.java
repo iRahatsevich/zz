82d4c28529ec50f5177a455303c611e4282a1c8b
iRahatsevich <irene.rahatsevich@gmail.com>
2021-02-06 13:16:34 -0600

    msgq

---
+++ src/main/java/Jiyde.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Jiyde {
+
+    public Jiyde(){
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numbers = sc.nextInt();
+        for (int i=1; i<=numbers; i++) {
+            for (int j=1; j<=numbers; j++) {
+                System.out.println("Hello from ");
+            }
+            System.out.println();
+        }
+
+        System.out.print("conflict  "+ numbers+numbers+numbers);
+
+
+
+    }
+}
---
+++ src/main/java/newclass.java
@@ -0,0 +1,11 @@
+
+
+public class newclass {
+    public static void main(String[] args) {
+        System.out.println("hello elvina" );
+        System.out.println("new linegit ");
+        System.out.println("lene 3");
+
+        System.out.println("anothere line ");
+    }
+}
